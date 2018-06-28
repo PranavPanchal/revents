94ee120ffe3e09851ea5f6a99a215370558bd4cc
Pranav Panchal <panchaldpranav@gmail.com>
2018-06-28 14:19:06 -0400

    configuring redux into app & restructuring

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { configureStore } from "./app/store/configureStore";
-import { createStore } from "redux";
+import ScrollToTop from "./app/common/util/ScrollToTop";
 
 const store = configureStore();
 
@@ -17,7 +17,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
 
