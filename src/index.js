65752a8d6b811fe0cd626a3d2fc3e43dc70af0d6
Pranav Panchal <panchaldpranav@gmail.com>
2018-06-28 11:41:11 -0400

    testing redux reducers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css';
-import './index.css';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { configureStore } from "./app/store/configureStore";
+import { createStore } from "redux";
 
-const rootEl = document.getElementById('root');
+const store = configureStore();
+
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>,
-        rootEl
-    );
-}
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+
+    rootEl
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    });
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
