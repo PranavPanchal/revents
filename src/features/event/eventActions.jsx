36eb5270b6daa361354a152f9e0019956e34e801
Pranav Panchal <panchaldpranav@gmail.com>
2018-07-16 10:41:38 -0400

    working on adding events

--- src/features/event/eventActions.jsx
+++ src/features/event/eventActions.jsx
@@ -1,4 +1,4 @@
-import { toastr } from 'react-redux-toastr'
+import { toastr } from "react-redux-toastr";
 import {
   CREATE_EVENT,
   DELETE_EVENT,
@@ -11,6 +11,7 @@ import {
   asyncActionError
 } from "../async/asyncActions";
 import { fetchSampleData } from "../../app/data/mockApi";
+import { createNewEvent } from "../../app/common/util/helpers";
 
 export const fetchEvents = events => {
   return {
@@ -20,17 +21,22 @@ export const fetchEvents = events => {
 };
 
 export const createEvent = event => {
-  return async dispatch => {
+  return async (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    const user = firestore.auth().currentUser;
+    const photoURL = getState.firebase.profile.photoURL;
+    let newEvent = createNewEvent(user, photoURL, event);
     try {
-      dispatch({
-        type: UPDATE_EVENT,
-        payload: {
-          event
-        }
+      let createdEvent = await firestore.add(`events`, newEvent);
+      await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+        eventId: createdEvent.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: true
       });
-      toastr.success('Success!', 'Event has been created')
+      toastr.success("Success!", "Event has been created");
     } catch (error) {
-      toastr.error('Oops', 'Something went wrong')
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
@@ -44,9 +50,9 @@ export const updateEvent = event => {
           event
         }
       });
-      toastr.success('Success!', 'Event has been updated')
+      toastr.success("Success!", "Event has been updated");
     } catch (error) {
-      toastr.error('Oops', 'Something went wrong')
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
